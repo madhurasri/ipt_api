632821a2ce863ee14a16ddd200b50f39c7212a58
madhurasri <yasithamadhu@gmail.com>
2020-04-08 17:44:47 +0530

    Added PUT and DELETE

--- accounts.php
+++ accounts.php
@@ -8,73 +8,114 @@ include 'core/config.php';
 $method = $_SERVER['REQUEST_METHOD'];
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
+$jwt=getBearerToken();
 
-	$jwt=getBearerToken();
+if ($jwt) {
 
-	if ($jwt) {
+	try {
+		JWT::$leeway = 10;
+	    $decoded = JWT::decode($jwt, SECRET_KEY, array(ALGORITHM));
+	    $user_role=$decoded->data->role;
 
-		try {
-			JWT::$leeway = 10;
-			    $decoded = JWT::decode($jwt, SECRET_KEY, array(ALGORITHM));
+	    if ($user_role=="admin") {
 
-			    // Access is granted.
+			// Access is granted.
 
-			    $member_id=isset($_GET['id']) ? $_GET['id'] : '';
-			    if (empty($member_id)) {
-			    	//get all members
-					$member_data = $database->select("user", [
-						"id",
-						"firstName",
-						"lastName",
-						"email",
-						"organization"
-					], [
-						"role" => "student",
-						"status" => 1
-					]);
-			    }else{
-			    	//get requested member
-					$member_data = $database->get("user", [
-						"id",
-						"firstName",
-						"lastName",
-						"email",
-						"organization"
-					], [
-						"id" => $member_id,
-						"role" => "student",
-						"status" => 1
-					]);		    	
-			    }
-			    
-			    if ($member_data) {
-			    	$data_insert=$member_data;
-			    }else{
+			if ($method == "GET") {
+				//get all new accounts
+				$account_data = $database->select("user", [
+					"id",
+					"firstName",
+					"lastName",
+					"email",
+					"organization"
+				], [
+					"role" => "student",
+					"status" => 0
+				]);
+
+				if ($account_data) {
+					$data_insert=$account_data;
+				}else{
 					$data_insert=array(
 					"status" => "success",
 					"message" => "No results."
 					);
-			    } 
+				}
+			}elseif ($method == "PUT") {
+				$account_id=isset($_GET['id']) ? $_GET['id'] : '';
+				if (!empty($account_id)) {
 
-		} catch (Exception $e){
+					$database->update("user", [
+						"status" => 1
+					], [
+						"id" => $account_id
+					]);
 
-			http_response_code(401);
+					$error=$database->error();
 
+					if ( empty( $error[1] ) ) {
+						$data_insert=array(
+							"status" => "success",
+							"message" => "Account approved successfully."
+						);	
+					}else{
+						$data_insert=array(
+							"status" => "error",
+							"message" => $error[2]
+						);			
+					}
+				}
+			}elseif ($method == "DELETE") {
+				$account_id=isset($_GET['id']) ? $_GET['id'] : '';
+				if (!empty($account_id)) {
+
+					$database->delete("user", [
+						"id" => $account_id
+					]);
+
+					$error=$database->error();
+
+					if ( empty( $error[1] ) ) {
+						$data_insert=array(
+							"status" => "success",
+							"message" => "Account deleted successfully."
+						);	
+					}else{
+						$data_insert=array(
+							"status" => "error",
+							"message" => $error[2]
+						);			
+					}
+				}
+			}
+
+ 
+
+	    }else{
 			$data_insert=array(
-				"jwt" => $jwt,
 				"status" => "error",
-				"message" => $e->getMessage()
+				"message" => "You don't have permission to access this data."
 			);
+	    }
 
-		}
+	} catch (Exception $e){
 
-	}else{
+		http_response_code(401);
 		$data_insert=array(
-			"data" => "0",
+			"jwt" => $jwt,
 			"status" => "error",
-			"message" => "Please request with access token."
+			"message" => $e->getMessage()
 		);
+
 	}
 
-header('Content-Type: application/json');
+}else{
+	$data_insert=array(
+		"status" => "error",
+		"message" => "Please request with access token."
+	);
+}
+
+//header('Content-Type: application/json');
 echo json_encode($data_insert);
